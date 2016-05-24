68528bba451e29329b8810bd6497adaec85aaeac
Oleg1983 <olegzinchenko1983@gmail.com>
2016-05-25 01:08:27 +0300

    Create structure. Slyce images

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+require('layout/header.php');
+ ?>
+
+<section class="promo">
+
+<div class="promo__inner">
+	
+</div>
+	
+
+</section>
+
+<section style="height: 500px;"></section>
+
+ <?php 
+require('layout/footer.php');
+  ?>
