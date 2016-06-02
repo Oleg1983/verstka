0dafacc7049de5a93c3e0491c752f5b836e3547b
Oleg1983 <olegzinchenko1983@gmail.com>
2016-06-02 21:47:16 +0300

    Subscribe section

--- index.php
+++ index.php
@@ -20,7 +20,84 @@ require('layout/header.php');
 
 </section>
 
-<section style="height: 500px;"></section>
+<section class="features">
+	<div class="container">
+
+		<div class="row">	
+
+				<div class="feature col-4 text-center">
+
+				<i class="feature__icon fa fa-cog"></i>
+
+				<h4 class="feature__title">Editable Theme</h4>
+				<p class="feature__content">
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</p>
+
+			</div>
+
+			<div class="feature col-4 text-center">
+				<i class="feature__icon fa fa-star"></i>
+				<h4 class="feature__title">Flat Design</h4>
+				<p class="feature__content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</p>
+			</div>
+
+			<div class="feature col-4 text-center">
+				<i class="feature__icon fa fa-globe"></i>
+				<h4 class="feature__title">Reach Your Audience</h4>
+				<p class="feature__content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+				</p>
+			</div>
+		</div>
+		
+		
+
+
+	</div>
+	
+
+
+</section>
+
+
+<section class="subscribe">
+
+	<div class="container">
+
+		<div class="row">
+
+			<div class="col-6">
+
+				<h3 class="subscribe__title">Get notified any updates!</h3>
+				<p class="subscribe_text">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa eligendi fugit tempora natus officiis doloremque voluptates architecto minus ut enim, ipsum dolor, error, asperiores ratione quos corporis tempore saepe commodi!
+				</p>
+				<form action="">
+					<fieldset class="subscribe__fieldset">
+						<input type="email" placeholder="Enter your email ...">
+						<button onclick="return false;">Notify</button>
+
+					</fieldset>
+
+				</form>
+			</div>	
+
+			<div class="col-6">
+
+				<div class="video__wrapper">
+					<iframe class="video__iframe" src="https://player.vimeo.com/video/168351281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+				</div>
+					
+			</div>
+
+
+		</div>		
+
+
+	</div>	
+
+</section>
 
  <?php 
 require('layout/footer.php');
